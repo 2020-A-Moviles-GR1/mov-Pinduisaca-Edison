27f7e2b22502751e83f8b398907b07be60892030
Edison Pinduisaca <edison.pinduisaca@epn.edu.ec>
2020-07-16 05:47:48 -0500

    ultima clase

--- 02-android/app/src/main/java/com/example/moviles/MainActivity.kt
+++ 02-android/app/src/main/java/com/example/moviles/MainActivity.kt
@@ -1,43 +1,45 @@
 package com.example.moviles
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.i("Activity", "OnCreate")
+        btn_ciclo_vida
+            .setOnClickListener { boton ->
+                // this.irCicloDeVida()
+                irCicloDeVida()
+            }
+
+        btn_list_view
+            .setOnClickListener { boton ->
+                irListView()
+            }
+
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.i("Activity", "OnStart")
+    fun irListView() {
+        val intentExplicito = Intent(
+            this,
+            BListViewActivity::class.java
+        )
+        // this.startActivity(intentExplicito)
+        startActivity(intentExplicito)
     }
 
-    override fun onRestart() {
-        super.onRestart()
-        Log.i("Activity", "OnRestart")
+    fun irCicloDeVida() {
+        val intentExplicito = Intent(
+            this,
+            CicloVida::class.java
+        )
+        // this.startActivity(intentExplicito)
+        startActivity(intentExplicito)
     }
 
-    override fun onResume() {
-        super.onResume()
-        Log.i("Activity", "OnResume")
-    }
 
-    override fun onPause() {
-        super.onPause()
-        Log.i("Activity","OnPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.i("Activity", "onStop")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.i("Activity", "onDestroy")
-    }
 }
